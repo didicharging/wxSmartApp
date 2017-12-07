cf940dbf288f3d2d1610a0f7c793fcfca6d79a56
didicharging <15088726293@139.com>
2017-12-07 09:35:42 +0800

    干掉了mapNav sideInfo ceshi userup账户升级页面 award

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 //app.js
 
 //let path = "https://appv2.didicharging.com/di";
-// let path = "http://localhost/di";
+ let path = "http://localhost/di";
 //let path ="https://app.liguangxu.top/di";
-let path = "https://api.didicharging.com/di";
+//let path = "https://api.didicharging.com/di";
 
 App({
   onLaunch: function () {
